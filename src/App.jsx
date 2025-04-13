eb8e18e5823d8dd6bbd483b4fc2fd27d050f5295
Derek Radcliffe <dereksradcliffe@gmail.com>
2025-04-13 14:14:45 -0400

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Header from "./components/Header";
+import MenuItem from "./components/MenuItem";
+function App() {
+  return (
+    <div className="App">
+      <Header />
+
+      <MenuItem />
+      <MenuItem />
+      <MenuItem />
+    </div>
+  );
+}
+
+
+export default App;
+
---
+++ src/components/MenuItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MenuItem = () => {
+    return (
+        <header>
+            <h1>Menu Item Component</h1>
+        </header>
+    );
+};
+
+export default MenuItem;
