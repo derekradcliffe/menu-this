788a89b9c72232c6a7eab27e0cd24c3d28b3fbbc
Derek Radcliffe <dereksradcliffe@gmail.com>
2025-04-14 12:08:52 -0400

    Added menu and updated menu items

---
+++ src/components/Cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./styles/Cart.css";
+
+const Cart = () => {
+    return (
+        <div className="cartWrapper">
+            <a href=""><img src="public\img\shopping_bag.svg" alt="Shopping Bag" /></a>
+        </div>
+    );
+};
+
+export default Cart;
